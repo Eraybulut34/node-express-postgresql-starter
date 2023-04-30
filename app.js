43ad536393f315e677c1cf98f4f89be6494afa53
eraybulut34 <abdullaheraybulut@hotmail.com>
2023-04-30 15:58:14 +0300

    cors config and test

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+//cors setup
+app.use(cors(
+  {
+    origin: 'http://localhost:8081',
+    credentials: true,
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }
+));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
