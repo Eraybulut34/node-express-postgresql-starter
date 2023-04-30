17847e220a7bc70ac9d584680acd6347c1195e67
eraybulut34 <abdullaheraybulut@hotmail.com>
2023-04-30 15:41:36 +0300

    init

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 const creds = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
+    username: "postgres",
+    password: "e1r2y3a4",
+    database: "postgres",
+    host: "localhost:5432",
     dialect: 'postgresql'
   },
   test: {
